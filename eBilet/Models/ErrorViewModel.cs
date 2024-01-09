5467a22cbb08a745a0e4a5ce84c8bf5e498a350d
jwasacz <kuba.wasacz160601@wp.pl>
2024-01-09 22:38:05 +0100

    Dodaj pliki projektów.

---
+++ eBilet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eBilet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
