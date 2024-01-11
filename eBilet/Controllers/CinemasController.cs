cca2fe9f84c6c3c1caff02a36f61737340a76ded
jwasacz <kuba.wasacz160601@wp.pl>
2024-01-11 20:28:10 +0100

    Commir&

--- eBilet/Controllers/CinemasController.cs
+++ eBilet/Controllers/CinemasController.cs
@@ -1,5 +1,12 @@
-﻿using eBilet.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using eBilet.Data;
+using eBilet.Models;
 
 namespace eBilet.Controllers
 {
@@ -7,16 +14,150 @@ namespace eBilet.Controllers
     {
         private readonly AppDbContext _context;
 
-
         public CinemasController(AppDbContext context)
         {
             _context = context;
         }
 
-        public IActionResult Index()
+        // GET: Cinemas
+        public async Task<IActionResult> Index()
         {
-            var allCinemas = _context.Cinemas.ToList();
-            return View(allCinemas);
+              return _context.Cinemas != null ? 
+                          View(await _context.Cinemas.ToListAsync()) :
+                          Problem("Entity set 'AppDbContext.Cinemas'  is null.");
+        }
+
+        // GET: Cinemas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Cinemas == null)
+            {
+                return NotFound();
+            }
+
+            var cinema = await _context.Cinemas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cinema == null)
+            {
+                return NotFound();
+            }
+
+            return View(cinema);
+        }
+
+        // GET: Cinemas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cinemas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Logo,Name,Description")] Cinema cinema)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cinema);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cinema);
+        }
+
+        // GET: Cinemas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Cinemas == null)
+            {
+                return NotFound();
+            }
+
+            var cinema = await _context.Cinemas.FindAsync(id);
+            if (cinema == null)
+            {
+                return NotFound();
+            }
+            return View(cinema);
+        }
+
+        // POST: Cinemas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Logo,Name,Description")] Cinema cinema)
+        {
+            if (id != cinema.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cinema);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CinemaExists(cinema.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cinema);
+        }
+
+        // GET: Cinemas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Cinemas == null)
+            {
+                return NotFound();
+            }
+
+            var cinema = await _context.Cinemas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cinema == null)
+            {
+                return NotFound();
+            }
+
+            return View(cinema);
+        }
+
+        // POST: Cinemas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Cinemas == null)
+            {
+                return Problem("Entity set 'AppDbContext.Cinemas'  is null.");
+            }
+            var cinema = await _context.Cinemas.FindAsync(id);
+            if (cinema != null)
+            {
+                _context.Cinemas.Remove(cinema);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CinemaExists(int id)
+        {
+          return (_context.Cinemas?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
